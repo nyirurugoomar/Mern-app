eefcdef3b90d64cd494db9215f3ecccfc6a38c04
nyirurugo omar <nyirurugoomar@gmail.com>
2023-08-30 19:06:06 +0200

    start working on api and there routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('Hello world')
+const express = require('express')
+const dontenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get('/api/goals', (req,res) =>{
+    res.status(200).json({ message:'Get goals' })
+})
+
+// app.post('/api/goals/:1',(req,res)=>{
+//     res.send('Post goals')
+// })
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`The server is running on port ${port}`)
+})
