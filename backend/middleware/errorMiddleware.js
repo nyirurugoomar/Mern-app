faef74aa2f1f77fc446b6c70a4c88a0cdd9d02f6
nyirurugo omar <nyirurugoomar@gmail.com>
2023-09-07 13:19:43 +0200

    initial REST API for goal

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,16 +1,16 @@
 const errorHandler = (err,req,res,next) => {
-    const statusCode = res.statusCode ? res.statusCode: 5000
+    const statusCode = res.statusCode ? res.statusCode: 5000;
 
     res.status(statusCode)
 
     res.json({
         message: err.message,
-        stack: processenv.NODEENV === 'production' ? null: err.stack 
-    })
+        stack: process.env.NODE_ENV === 'production' ? null: err.stack,
+    });
 
-}
+};
 
 
 module.exports = {
     errorHandler,
-}
+};
